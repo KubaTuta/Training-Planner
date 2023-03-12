5a14df88dca89cd8ed0bc52bf2fb1102a0eebdb4
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-12 16:23:16 +0100

    local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import AddNewExc from "./AddNewExc";
 import RenderExcercises from "./RenderExc";
@@ -7,58 +7,74 @@ import RenderSession from "./RenderSession";
 
 function App() {
 
-  const initialTasks = [
-    {
-      id: 1,
-      excercise: "planche lean",
-      session1: {
-        id: 1,
-        sets: "",
-        reps: 100,
-        time: "",
-      },
-      session2: {
-        id: 2,
-        sets: "",
-        reps: "",
-        time: 100
-      }
-    },
-    {
-      id: 2,
-      excercise: "dips",
-      session1: {
-        id: 1,
-        sets: 100,
-        reps: "",
-        time: 100,
-      },
-      session2: {
-        id: 2,
-        sets: 100,
-        reps: 100,
-        time: ""
-      }
-    },
-    {
-      id: 3,
-      excercise: "pushups",
-      session1: {
-        id: 1,
-        sets: 100,
-        reps: "",
-        time: "",
-      },
-      session2: {
-        id: 2,
-        sets: "",
-        reps: "",
-        time: ""
-      }
-    },
-  ];
+  const [newDate, setNewDate] = useState("");
 
-  const [tasks, setTasks] = useState(initialTasks);
+  // const initialTasks = [
+  //   {
+  //     id: 1,
+  //     excercise: "planche lean",
+  //     session1: {
+  //       id: 1,
+  //       sets: "",
+  //       reps: 100,
+  //       time: "",
+  //       date: "2022-12-31"
+  //     },
+  //     session2: {
+  //       id: 2,
+  //       sets: "",
+  //       reps: "",
+  //       time: 100,
+  //       date: "2023-01-01"
+  //     }
+  //   },
+  //   {
+  //     id: 2,
+  //     excercise: "dips",
+  //     session1: {
+  //       id: 1,
+  //       sets: 100,
+  //       reps: "",
+  //       time: 100,
+  //       date: "2022-12-31"
+  //     },
+  //     session2: {
+  //       id: 2,
+  //       sets: 100,
+  //       reps: 100,
+  //       time: "",
+  //       date: "2023-01-01"
+  //     }
+  //   },
+  //   {
+  //     id: 3,
+  //     excercise: "pushups",
+  //     session1: {
+  //       id: 1,
+  //       sets: 100,
+  //       reps: "",
+  //       time: "",
+  //       date: "2022-12-31"
+  //     },
+  //     session2: {
+  //       id: 2,
+  //       sets: "",
+  //       reps: "",
+  //       time: "",
+  //       date: "2023-01-01"
+  //     }
+  //   },
+  // ];
+  const downloadedWorkout = localStorage.getItem("workout");
+
+  const [tasks, setTasks] = useState(downloadedWorkout ? JSON.parse(downloadedWorkout) : []);
+
+  
+
+
+  useEffect(()=> {
+    localStorage.setItem("workout", JSON.stringify(tasks))
+  }, [tasks]);
 
   console.log(tasks);
 
@@ -68,11 +84,14 @@ function App() {
       <AddNewExc
         setTasks={setTasks}
         tasks={tasks}
+        newDate={newDate}
       />
 
       <AddSession
       tasks={tasks}
       setTasks={setTasks}
+      newDate={newDate}
+      setNewDate={setNewDate}
       
       />
 
