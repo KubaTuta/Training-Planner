593a28430ae2c781beec22ab7396117d55c84379
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-31 14:08:37 +0200

    redux logic

--- src/Features/workoutSlice.js
+++ src/Features/workoutSlice.js
@@ -3,12 +3,67 @@ import { createSlice } from "@reduxjs/toolkit";
 const workoutSlice = createSlice({
   name: "workouts",
   initialState: {
-    tasks: "",
+    tasks: [
+      {
+        id: 1,
+        excercise: "planche lean",
+        session1: {
+          id: 1,
+          sets: "",
+          reps: 100,
+          time: "",
+          date: "2022-12-31"
+        },
+        session2: {
+          id: 2,
+          sets: "",
+          reps: "",
+          time: 100,
+          date: "2023-01-01"
+        }
+      },
+      {
+        id: 2,
+        excercise: "dips",
+        session1: {
+          id: 1,
+          sets: 100,
+          reps: "",
+          time: 100,
+          date: "2022-12-31"
+        },
+        session2: {
+          id: 2,
+          sets: 100,
+          reps: 100,
+          time: "",
+          date: "2023-01-01"
+        }
+      },
+      {
+        id: 3,
+        excercise: "pushups",
+        session1: {
+          id: 1,
+          sets: 100,
+          reps: "",
+          time: "",
+          date: "2022-12-31"
+        },
+        session2: {
+          id: 2,
+          sets: "",
+          reps: "",
+          time: "",
+          date: "2023-01-01"
+        }
+      },
+    ]
   },
   reducers: {
     addExcercise: (state, action) => {
-      const newTask  = action.payload;
-
+      const newTask = action.payload;
+      const date = (new Date()).toLocaleString('pl-PL', { year: 'numeric', month: '2-digit', day: '2-digit' }).replace(/\./g, '-');
       if (state.tasks.length === 0) {
 
         return {
@@ -23,7 +78,8 @@ const workoutSlice = createSlice({
                 sets: "",
                 reps: "",
                 time: "",
-                
+                date: date,
+
               },
             },
           ],
@@ -46,21 +102,99 @@ const workoutSlice = createSlice({
                 sets: "",
                 reps: "",
                 time: "",
-                
+                date: date,
+
               }
             }]
         }
 
           ;
       }
-    }
+    },
+    addSession: (state, action) => {
+      const newDate = action.payload;
+      state.tasks = state.tasks.map(excercise => {
+        const arrayExcercise = Object.values(state.tasks[0]);
+        const lastPosition = Object.keys(state.tasks[0]).length - 1;
+        const newId = arrayExcercise[lastPosition].id + 1;
+        const sessionName = "session" + newId;
+
+        if (Object.keys(state.tasks[0]).length < 3) {
+          return {
+            ...excercise,
+            session1: {
+              id: 1,
+              sets: "",
+              reps: "",
+              time: "",
+              date: newDate,
+            }
+          }
+        }
+        return {
+          ...excercise,
+          [sessionName]: {
+            id: newId,
+            sets: "",
+            reps: "",
+            time: "",
+            date: newDate,
+          }
+        }
+      })
+
+
+    },
+    removeSession: (state, action) => {
+      const id = action.payload;
+      const sessionName = "session" + (id);
+      state.tasks = state.tasks.map(task => {
+        const { [sessionName]: beingRemovedSession, ...rest } = task;
+        return rest
+      })
+    },
+    addActivity: (state, action) => {
+      const { session, excerciseId, activity } = action.payload;
+      const promptValue = prompt("podaj ilość serii");
+      const sessionName = "session" + (session);
+
+      state.tasks = state.tasks.map(excercise => {
+        if (excercise.id === excerciseId) {
+          return {
+            ...excercise,
+            [sessionName]: {
+              ...excercise[sessionName],
+              [activity]: promptValue,
+            }
+          }
+        }
+        return excercise;
+      })
+    },
+    removeActivity: (state, action) => {
+      const { session, excerciseId, activity } = action.payload;
+      const sessionName = "session" + (session);
+      console.log(session);
+      state.tasks = state.tasks.map(excercise => {
+        if (excercise.id === excerciseId) {
+          return {
+            ...excercise,
+            [sessionName]:
+            {
+              ...excercise[sessionName],
+              [activity]: "",
+            }
+          }
+        }
+        return excercise
+      }
+      )
+    },
   }
 });
 
-export const { countTasks, addExcercise } = workoutSlice.actions;
+export const { addExcercise, addSession, removeSession, addActivity, removeActivity } = workoutSlice.actions;
 
-export const selectWorkoutsState = state => state.workouts;
-export const selectWorkouts = state => selectWorkoutsState(state).workout;
-console.log(selectWorkoutsState);
-
+export const selectWorkoutsState = state => state.workout;
+export const selectWorkouts = state => selectWorkoutsState(state).tasks;
 export default workoutSlice.reducer;
