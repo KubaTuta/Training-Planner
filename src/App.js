b08ea21b4c5c119adc188364d88a6407804ae0be
Kuba Tuta <tuta.jakub@gmail.com>
2023-03-31 20:24:31 +0200

    removing initial, abundant localStorage and props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./index.css";
 import AddNewExc from "./Features/AddNewExc";
 import RenderExcercises from "./Features/RenderExc";
@@ -9,101 +9,17 @@ function App() {
 
   const [newDate, setNewDate] = useState("");
 
-  // const initialTasks = [
-  //   {
-  //     id: 1,
-  //     excercise: "planche lean",
-  //     session1: {
-  //       id: 1,
-  //       sets: "",
-  //       reps: 100,
-  //       time: "",
-  //       date: "2022-12-31"
-  //     },
-  //     session2: {
-  //       id: 2,
-  //       sets: "",
-  //       reps: "",
-  //       time: 100,
-  //       date: "2023-01-01"
-  //     }
-  //   },
-  //   {
-  //     id: 2,
-  //     excercise: "dips",
-  //     session1: {
-  //       id: 1,
-  //       sets: 100,
-  //       reps: "",
-  //       time: 100,
-  //       date: "2022-12-31"
-  //     },
-  //     session2: {
-  //       id: 2,
-  //       sets: 100,
-  //       reps: 100,
-  //       time: "",
-  //       date: "2023-01-01"
-  //     }
-  //   },
-  //   {
-  //     id: 3,
-  //     excercise: "pushups",
-  //     session1: {
-  //       id: 1,
-  //       sets: 100,
-  //       reps: "",
-  //       time: "",
-  //       date: "2022-12-31"
-  //     },
-  //     session2: {
-  //       id: 2,
-  //       sets: "",
-  //       reps: "",
-  //       time: "",
-  //       date: "2023-01-01"
-  //     }
-  //   },
-  // ];
-  const downloadedWorkout = localStorage.getItem("workout");
-
-  const [tasks, setTasks] = useState(downloadedWorkout ? JSON.parse(downloadedWorkout) : []);
-
-  
-
-
-  useEffect(()=> {
-    localStorage.setItem("workout", JSON.stringify(tasks))
-  }, [tasks]);
-
-  console.log(tasks);
-
   return (
     <div className="layout">
-
       <AddNewExc
-        setTasks={setTasks}
-        tasks={tasks}
         newDate={newDate}
       />
-
       <AddSession
-      tasks={tasks}
-      setTasks={setTasks}
-      newDate={newDate}
-      setNewDate={setNewDate}
-      
+        newDate={newDate}
+        setNewDate={setNewDate}
       />
-
-      <RenderExcercises
-        tasks={tasks}
-        setTasks={setTasks}
-
-      />
-      <RenderSession
-        tasks={tasks}
-        setTasks={setTasks}
-      />
+      <RenderExcercises />
+      <RenderSession />
     </div>
 
   );
