c9844fbfb8833b4cc212b6f8a8e2cd54174487f3
Kuba Tuta <tuta.jakub@gmail.com>
2023-04-26 09:59:50 +0200

    saga for whole units

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,8 @@
 import { all } from "redux-saga/effects";
-import { workoutSaga } from "./Features/workoutSaga";
+import { unitSaga } from "./Features/Home/unitSaga";
 
 export default function* rootSaga() {
   yield all([
-    workoutSaga(),
+    unitSaga(),
   ])
 };
