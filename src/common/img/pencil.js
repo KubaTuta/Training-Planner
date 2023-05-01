95545c187e9c3a90e0bd527b1c29f1e7e17c44fd
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-01 10:23:42 +0200

    edit svg

---
+++ src/common/img/pencil.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {ReactComponent as pencil} from "./pencil.svg";
+
+export const Pencil = styled(pencil)`
+  margin: 0;
+  path {
+    fill: #ffffffb3
+  }
+`;
