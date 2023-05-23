10542d1d63262f17020664f44f06680391844b9b
Kuba Tuta <tuta.jakub@gmail.com>
2023-05-23 13:27:46 +0200

    custom hook for modals

---
+++ src/common/hooks/useModal.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export const useModal = () => {
+  const [modal, setModal] = useState({
+    modalState: false,
+    editState: false,
+    removeState: false,
+    modalId: undefined,
+  });
+
+  const toggleModal = (id) => {
+    setModal({
+      modalState: !modal.modalState,
+      modalId: id,
+    });
+  };
+
+  const toggleEditModal = (id) => {
+    setModal({
+      editState: !modal.editState,
+      modalId: id,
+    });
+  };
+
+  const toggleRemoveModal = (id) => {
+    setModal({
+      removeState: !modal.removeState,
+      modalId: id,
+    });
+  };
+
+  return { modal, setModal, toggleModal, toggleEditModal, toggleRemoveModal };
+};
